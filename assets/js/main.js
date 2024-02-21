43b8d0126296612271f6a555496201ba3b0ec40c
marinodilauro <m.dilauro90@gmail.com>
2024-02-21 14:44:12 +0100

    Create HTML element variable and loop to generate numbers

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Creo delle variabili e assegno loro l'elemento HTML in cui voglio creare altri elementi HTML tramite JS
+
+const listElement = document.querySelector(".list");
+console.log(listElement);
+
+// Creo un ciclo for per stampare in console i numeri da 1 a 100.
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+
+//Verifico se i numeri generati sono multipli di 3, di 5, o di entrambi.
