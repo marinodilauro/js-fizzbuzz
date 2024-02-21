fd607fd49876854af6192389097918cebb32fd07
marinodilauro <m.dilauro90@gmail.com>
2024-02-21 15:17:38 +0100

    Create variables for special outputs and add outputs on screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,38 @@
 const listElement = document.querySelector(".list");
 //console.log(listElement);
 
+// Creo variabili per gli output speciali
+
+const fizz = "Fizz";
+const buzz = "Buzz";
+const fizzbuzz = "FizzBuzz";
+
+
 // Creo un ciclo for per stampare in console i numeri da 1 a 100.
 
 for (let i = 1; i <= 100; i++) {
 
   //Verifico se i numeri generati sono multipli di 3, di 5, o di entrambi e stampo il corrispettivo output
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+
+    //console.log(fizzbuzz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${fizzbuzz}</li>`);
+
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+
+    //console.log(fizz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${fizz}</li>`);
+
   } else if (i % 5 == 0) {
-    console.log("Buzz");
+
+    //console.log(buzz);
+    listElement.insertAdjacentHTML("beforeend", `<li>${buzz}</li>`);
+
   } else {
-    console.log(i)
+
+    // console.log(i)
+    listElement.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
+
   };
 
 };
