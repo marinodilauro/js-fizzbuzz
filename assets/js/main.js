bc323fab42c897810dad28eed68dd4f4eb62d785
marinodilauro <m.dilauro90@gmail.com>
2024-02-21 16:09:25 +0100

    Update JS comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,16 @@
-// Creo delle variabili e assegno loro l'elemento HTML in cui voglio creare altri elementi HTML tramite JS
+// Create variable and assign them HTML element
 
 const rowElement = document.querySelector(".row");
-//console.log(listElement);
 
-// Creo variabili per gli output speciali
+
+// Create variables for special oputputs
 
 const fizz = "Fizz";
 const buzz = "Buzz";
 const fizzbuzz = "FizzBuzz";
 
 
-
-
-// Creo un ciclo for per stampare in console i numeri da 1 a 100.
+// Create for loop to generate numbers from 1 to 100
 
 for (let i = 1; i <= 100; i++) {
 
@@ -21,25 +19,21 @@ for (let i = 1; i <= 100; i++) {
   const buzzSquare = `<div class="mc_square mc_buzz rounded text-center fw-bold">${buzz}</div>`;
   const fizzBuzzSquare = `<div class="mc_square mc_fizzbuzz rounded text-center fw-bold">${fizzbuzz}</div>`;
 
-  //Verifico se i numeri generati sono multipli di 3, di 5, o di entrambi e stampo il corrispettivo output
+  // Verify if generated numbers are multiple of 3, 5 or both and show ooutputs on screen
   if (i % 3 == 0 && i % 5 == 0) {
 
-    //console.log(fizzbuzz);
     rowElement.insertAdjacentHTML("beforeend", fizzBuzzSquare);
 
   } else if (i % 3 == 0) {
 
-    //console.log(fizz);
     rowElement.insertAdjacentHTML("beforeend", fizzSquare);
 
   } else if (i % 5 == 0) {
 
-    //console.log(buzz);
     rowElement.insertAdjacentHTML("beforeend", buzzSquare);
 
   } else {
 
-    // console.log(i)
     rowElement.insertAdjacentHTML("beforeend", square);
 
   };
