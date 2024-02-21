e0acf71c672faaf1075da87fea67a84b7ac941de
marinodilauro <m.dilauro90@gmail.com>
2024-02-21 15:11:29 +0100

    Create for loop with conditional statements to verify if a number is multiple of 3, 5 or both.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 // Creo delle variabili e assegno loro l'elemento HTML in cui voglio creare altri elementi HTML tramite JS
 
 const listElement = document.querySelector(".list");
-console.log(listElement);
+//console.log(listElement);
 
 // Creo un ciclo for per stampare in console i numeri da 1 a 100.
 
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
-}
+
+  //Verifico se i numeri generati sono multipli di 3, di 5, o di entrambi e stampo il corrispettivo output
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i)
+  };
+
+};
 
 
-//Verifico se i numeri generati sono multipli di 3, di 5, o di entrambi.
